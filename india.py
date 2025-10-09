0033c0730ed2b806c55a5ea834bd5cb27c025cb4
hehesnortsnort6969-png <hehesnortsnort6969@gmail.com>
2025-10-09 08:17:14 -0400

    Update india.py

--- india.py
+++ india.py
@@ -2,4 +2,5 @@ import streamlit as st
 import pandas as pd
 st.title("top 10 reasons why John is retarded")
 names = ("John","Nathan","Moe")
-st.selectbox("names",names)
+selectbox = st.selectbox("names",names)
+st.write(selectbox)
