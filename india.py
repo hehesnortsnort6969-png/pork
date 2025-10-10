878b160aa847aeedbeaad540ff39d891a36f47bb
hehesnortsnort6969-png <hehesnortsnort6969@gmail.com>
2025-10-09 20:31:51 -0400

    Update india.py

--- india.py
+++ india.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.title("top 10 reasons why John is retarded")
-data = pd.read_csv("")
+data = pd.read_csv("india.csv")
 selectbox = st.selectbox("data",names)
 st.write(selectbox)
